ed6bff89210faaff26fa4e441b04f524aed29194
polina novozhilova <79155735+blurrechka@users.noreply.github.com>
2022-05-16 17:21:07 +0300

    tehtava 1.6-1.11

--- App.js
+++ App.js
@@ -1,53 +1,91 @@
-const course = 'Half Stack application development'
-const part1 = 'Fundamentals of React'
-const exercises1 = 10
-const part2 = 'Using props to pass data'
-const exercises2 = 7
-const part3 = 'State of a component'
-const exercises3 = 14
-const total = 'Number of exercises'
-const totaln = exercises1 + exercises2 + exercises3
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
 
 const Header = (props) => {
   return (
     <div>
-      <p>{props.course}</p>
-    </div>
-  )
-}
-const Part = (props) => {
-  return (
-    <div>
-      {props.part} {props.exercises}
+      <h1>{props.header}</h1>
     </div>
   )
 }
 
-const Content = () => {
+const Button = ( {handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
+)
+
+
+
+const History = (props) => {
+  if(props.total === 0) {
+    return <p>No feedback given</p>
+  }
   return (
-    <div>
-      <Part part = {part1} exercises = {exercises1}/>
-      <Part part = {part2} exercises = {exercises2}/>
-      <Part part = {part3} exercises = {exercises3}/>
-    </div>
+    <table>
+      <tbody>
+        <Statistic text="good" value={props.good} />
+        <Statistic text="neutral" value={props.nautral} />
+        <Statistic text="bad" value={props.bad} />
+        <Statistic text="all" value={props.total} />
+        <Statistic text="average" value={props.average} />
+        <Statistic text="positive" value={props.posotove} />
+      </tbody>
+    </table>
+  )
+}
+const Statistic = ({ text, value }) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
   )
 }
 
-const Total = (props) => {
-  return (
-    <div>
-      <p> {props.total} {props.totaln}</p>
-    </div>
-  )
-}
 const App = () => {
+
+  const header = 'Give feedback'
+
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const total = good + neutral + bad
+  const average = ((good + bad * -1) / total).toFixed(2)
+  const positive = ((good / total) * 100).toFixed(2).toString(10) + " %"
+
+  const handleGood = () => {
+    setGood(good + 1)
+  }
+
+  const handleNeutral = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const handleBad = () => {
+    setBad(bad + 1)
+  }
+
   return (
     <div>
-      <Header course = {course} />
-      <Content />
-      <Total total = {total} totaln = {totaln}/>
+      <div>
+        <Header header = {header} />
+        <Button handleClick={handleGood} text = 'good'/>
+        <Button handleClick={handleNeutral} text = 'neutral'/>
+        <Button handleClick={handleBad} text = 'bad'/>
+      </div>
+      <div>
+        <h1>Statistic</h1>
+        <History
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={total}
+          average={average}
+          posotove={positive}
+        />
+      </div>
     </div>
   )
 }
+
 
 export default App
